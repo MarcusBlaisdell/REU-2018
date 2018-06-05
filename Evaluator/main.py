ad3dcb2a49f99f3579f74f62723998c852b76c0d
mblaisde <marcus.blaisdell@wsu.edu>
2018-06-05 16:26:25 -0700

    updated main

--- Evaluator/main.py
+++ Evaluator/main.py
@@ -11,118 +11,132 @@ from functions import *
 myPGClass = pg_Class()
 myPRClass = pr_Class()
 
-### create arrays with all the training, testing files:
+### create array of the values to label the training, testing, results files:
 
-trainArray = ["11a-train", "11b-train", "11c-train", "11d-train", \
-                "13a-train", "13b-train", "13c-train", "13d-train", \
-                "15a-train", "15b-train", "15c-train", "15d-train", \
-                "17a-train", "17b-train", "17c-train", "17d-train" ]
-
-testArray = ["11a-test", "11b-test", "11c-test", "11d-test", \
-                "13a-test", "13b-test", "13c-test", "13d-test", \
-                "15a-test", "15b-test", "15c-test", "15d-test", \
-                "17a-test", "17b-test", "17c-test", "17d-test" ]
-
-resultsArray = ["11a", "11b", "11c", "11d", \
-                "13a", "13b", "13c", "13d", \
-                "15a", "15b", "15c", "15d", \
-                "17a", "17b", "17c", "17d" ]
+kmerList = [11, 13, 15, 17]
+subList = ['a', 'b', 'c', 'd']
 
 ### run the comparison for each training/testing set:
 
-thePath = "/home/marcus/Data/Genomic-Data/Testing/"
+#thePath = "/home/marcus/Data/Genomic-Data/Testing/"
+thePath = "/data/doppa/users/mblaisdell/genome-assembly-datasets/Testing/"
 resultsStringPre = "results_"
 resultsStringPost = ".csv"
 
-for index in range(len(trainArray)):
-    trainData = thePath + trainArray[index]
-    testData = thePath + testArray[index]
-    resultsFile = resultsStringPre + resultsArray[index] + resultsStringPost
+### create empty status file:
+statusFile = open("status.txt", "w")
+statusFile.close()
 
-    print 'set: ', resultsArray[index]
+for kmer in kmerList:
+    myPGClass.kmer = kmer
+    for sub in subList:
+        trainData = thePath + str(kmer) + sub + '-train'
+        testData = thePath + str(kmer) + sub + '-test'
+        resultsFile = resultsStringPre + str(kmer) + sub + resultsStringPost
 
-    print 'loading data for perceptron:'
-    loadData(trainData, myPRClass.trainData)
-    loadData(testData, myPRClass.testData)
-    print 'load complete'
-    myPRClass.countGood()
+        statusFile = open ("status.txt", "a")
+        print 'set: ', str(kmer) + sub
+        statusFile.write('set: ' + str(str(kmer) + sub) + '\n')
 
-    print 'opening results file:'
-    outFile = open(resultsFile, "w")
+        print 'loading data for perceptron:'
+        statusFile.write ('loading data for perceptron:' + '\n')
+        loadData(trainData, myPRClass.trainData)
+        loadData(testData, myPRClass.testData)
+        print 'load complete'
+        statusFile.write ('load complete' + '\n')
+        myPRClass.countGood()
 
-    outFile.write ('Perceptron'  + '\n' + '\n')
-    outFile.write('type' + ',' + 'Precision' + ',' + 'Recall' + ',' + 'F1' + ',' + \
-                  'Accuracy' + ',' + 'T' + ',' + 'K' + ',' \
-                  + 'Mistakes' + ',' + 'Iteration' + '\n')
+        print 'opening results file:'
+        statusFile.write ('opening results file:' + '\n')
+        outFile = open(resultsFile, "w")
 
-    print 'running perceptron . . . '
+        outFile.write ('Perceptron'  + '\n' + '\n')
+        outFile.write('type' + ',' + 'Precision' + ',' + 'Recall' + ',' + 'F1' + ',' + \
+                      'Accuracy' + ',' + 'T' + ',' + 'K' + ',' \
+                      + 'Mistakes' + ',' + 'Iteration' + '\n')
 
-    startTime = time.time()
-    myPRClass.perceptron(outFile)
-    endTime = time.time()
-    print 'Perceptron runTime: ', endTime - startTime
-    outFile.write (str(endTime - startTime))
+        print 'running perceptron . . . '
+        statusFile.write ('running perceptron . . . ' + '\n')
+        statusFile.close()
 
-    ### end test perceptron
+        startTime = time.time()
+        myPRClass.perceptron(outFile)
+        endTime = time.time()
+        print 'Perceptron runTime: ', endTime - startTime
+        outFile.write (str(endTime - startTime))
+        statusFile = open("status.txt", "a")
 
-    print 'loading data for Pegasos . . . '
+        ### end test perceptron
 
-    loadData(trainData, myPGClass.trainData)
-    loadData(testData, myPGClass.testData)
+        print 'loading data for Pegasos . . . '
+        statusFile.write ('loading data for Pegasos . . . ' + '\n')
 
-    print 'load complete'
+        loadData(trainData, myPGClass.trainData)
+        loadData(testData, myPGClass.testData)
 
-    myPGClass.countGood()
+        print 'load complete'
+        statusFile.write ('load complete' + '\n')
 
-
-    ### Add a header for Pegasos:
-    outFile.write ('\n' + '\n' + 'Pegasos' + '\n' + '\n')
-    outFile.write('type' + ',' + 'Precision' + ',' + 'Recall' + ',' + 'F1' + ',' + \
-                  'Accuracy' + ',' + 'T' + ',' + 'K' + ',' + 'Lamda' + ',' \
-                  + 'Mistakes' + ',' + 'Iteration' + '\n')
-
-    print 'running Pegasos . . .'
-
-    ### record start time to determine runtime of algorithm
-    startTime = time.time()
-
-    ### run for each value of T:
-    for i in myPGClass.Tlist:
-        myPGClass.T = i
-        ### run for each value of k:
-        for j in myPGClass.klist:
-            myPGClass.k = j
-            for l in myPGClass.lamList:
-                myPGClass.lam = l
-
-                # initialize weight vector, w to zero:
-
-                kmerSize = ((4**myPGClass.kmer) + 1)
-                myPGClass.w = np.zeros(kmerSize, dtype=float)
-                #myPGClass.w = np.zeros(10000000, dtype = float)
-
-                ### repeat for T iterations:
-                for t in range(myPGClass.T):
-                    myPGClass.pegasosBatch(t)
-
-                    outFile.write('train' + ',' + str(myPGClass.trainPrecision) + ',' + str(myPGClass.trainRecall) \
-                                  + ',' + str(myPGClass.trainF1) + ',' + str(myPGClass.trainAccuracy)\
-                                  + ',' + str(myPGClass.T) + ',' + str(myPGClass.k) + ',' \
-                                  + str(myPGClass.lam) + ',' + str(myPGClass.trainMistakes) + ',' + str(t + 1) + '\n')
-
-                    myPGClass.testWeight()
-                    outFile.write('test' + ',' + str(myPGClass.testPrecision) + ',' + str(myPGClass.testRecall) \
-                                  + ',' + str(myPGClass.testF1) + ',' + str(myPGClass.testAccuracy)\
-                                  + ',' + str(myPGClass.T) + ',' + str(myPGClass.k) + ',' \
-                                  + str(myPGClass.lam) + ',' + str(myPGClass.testMistakes) + '\n')
+        myPGClass.countGood()
 
 
-    endTime = time.time()
-    print 'Pegasos runTime: ', endTime - startTime
-    outFile.write (str(endTime - startTime))
+        ### Add a header for Pegasos:
+        outFile.write ('\n' + '\n' + 'Pegasos' + '\n' + '\n')
+        outFile.write('type' + ',' + 'Precision' + ',' + 'Recall' + ',' + 'F1' + ',' + \
+                      'Accuracy' + ',' + 'T' + ',' + 'K' + ',' + 'Lamda' + ',' \
+                      + 'Mistakes' + ',' + 'Iteration' + '\n')
 
-    print 'closing output file . . .'
+        print 'running Pegasos . . .'
+        statusFile.write('running Pegasos . . .' + '\n')
+        statusFile.close()
 
-    outFile.close()
+        ### record start time to determine runtime of algorithm
+        startTime = time.time()
+
+        ### run for each value of T:
+        for i in myPGClass.Tlist:
+            myPGClass.T = i
+            ### run for each value of k:
+            for j in myPGClass.klist:
+                myPGClass.k = j
+                for l in myPGClass.lamList:
+                    myPGClass.lam = l
+
+                    # initialize weight vector, w to zero:
+
+                    kmerSize = ((4**myPGClass.kmer) + 1)
+                    myPGClass.w = np.zeros(kmerSize, dtype=float)
+                    #myPGClass.w = np.zeros(10000000, dtype = float)
+
+                    ### repeat for T iterations:
+                    for t in range(myPGClass.T):
+                        myPGClass.pegasosBatch(t)
+
+                        outFile.write('train' + ',' + str(myPGClass.trainPrecision) + ',' + str(myPGClass.trainRecall) \
+                                      + ',' + str(myPGClass.trainF1) + ',' + str(myPGClass.trainAccuracy)\
+                                      + ',' + str(myPGClass.T) + ',' + str(myPGClass.k) + ',' \
+                                      + str(myPGClass.lam) + ',' + str(myPGClass.trainMistakes) + ',' + str(t + 1) + '\n')
+
+                        myPGClass.testWeight()
+                        outFile.write('test' + ',' + str(myPGClass.testPrecision) + ',' + str(myPGClass.testRecall) \
+                                      + ',' + str(myPGClass.testF1) + ',' + str(myPGClass.testAccuracy)\
+                                      + ',' + str(myPGClass.T) + ',' + str(myPGClass.k) + ',' \
+                                      + str(myPGClass.lam) + ',' + str(myPGClass.testMistakes) + '\n')
+
+
+        endTime = time.time()
+        print 'Pegasos runTime: ', endTime - startTime
+        outFile.write (str(endTime - startTime))
+        statusFile = open("status.txt", "a")
+        statusFile.write('Pegasos runTime: ' + str(endTime - startTime) + '\n')
+
+        print 'closing output file . . .'
+        statusFile.write('closing output file . . .' + '\n')
+        statusFile.close()
+
+        outFile.close()
 
 print '*** program complete ***'
+statusFile = open("status.txt", "a")
+statusFile.write('*** program complete ***' + '\n')
+statusFile.close()
